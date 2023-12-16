515cc8ba5c5774b64eb261a3d328281f32a24909
Zachary Opseth <zachary.opseth@gmail.com>
2023-12-16 08:02:46 -0600

    Revert "Add Node adapter to run properly on GitHub Pages"
    This reverts commit 5828f15b81241d5e0ed861ab7012a9f7a787bb28.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,6 @@ import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { ANALYTICS, SITE } from './src/utils/config.ts';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) =>
   ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
@@ -28,9 +26,6 @@ export default defineConfig({
   base: SITE.base,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,

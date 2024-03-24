7e75840d18f996e3cb1f1bff0460a43728b4735e
Brian Phillips <28457+brianphillips@users.noreply.github.com>
2024-03-23 22:24:13 -0500

    change method of knowing if there are any classes available to register for

--- src/utils/registrationIsOpen.ts
+++
@@ -1,6 +0,0 @@
-import { getCurrentSemester } from '~/queries/Semester';
-
-export const getRegistrationStatus = async (): Promise<boolean> => {
-  const currentSemester = await getCurrentSemester();
-  return currentSemester != null;
-};

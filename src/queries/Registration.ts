e2064d01015287f44a2124fdde37317b4da2eb1c
Brian Phillips <28457+brianphillips@users.noreply.github.com>
2024-03-24 19:35:39 -0500

    clean up the registration table

--- src/queries/Registration.ts
+++ src/queries/Registration.ts
@@ -1,12 +1,12 @@
-import groq from 'groq';
 import { makeDataAccess, type BaseQueryOptions } from './sanity';
 import { CLASS_QUERY_FRAGMENT, parseRawClassArray } from './Classes';
 
-const isRegistrationOpenQuery = groq`
-count(*[_type == "class" && coalesce(registration_open, semester.registration_open) < now() && coalesce(registration_close, coalesce(dates.start, semester.dates.start)) > now()]) > 0
-`;
+const OPEN_CLASS_RESTRICTION_FRAGMENT = `*[_type == "class" && coalesce(registration_open, semester.registration_open) < now() && coalesce(registration_close, coalesce(dates.start, semester.dates.start)) > now()]`;
+
+const isRegistrationOpenQuery = `count(${OPEN_CLASS_RESTRICTION_FRAGMENT}) > 0`;
+
 const classesOpenForRegistrationQuery = (options: BaseQueryOptions) => `
-*[_type == "class" && coalesce(registration_open, semester.registration_open) < now() && coalesce(registration_close, coalesce(dates.start, semester.dates.start)) > now()] | order(coalesce(registration_close, coalesce(dates.start, semester->.dates.start)) asc, class_times.start)
+${OPEN_CLASS_RESTRICTION_FRAGMENT} | order(coalesce(registration_close, coalesce(dates.start, semester->.dates.start)) asc, class_times.start)
   ${CLASS_QUERY_FRAGMENT(options)}
 `;
 

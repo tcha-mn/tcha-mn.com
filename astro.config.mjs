129b787c9a746861eaa0a06d9e81d60c25f6b212
Brian Phillips <28457+brianphillips@users.noreply.github.com>
2026-01-28 11:23:23 -0600

    upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import tasks from './src/utils/tasks.mjs';
 import { ANALYTICS, SITE, SANITY } from './src/utils/config.ts';
 import sanityIntegration from '@sanity/astro';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 const CURRENT_THEATRE_SEASON = '2025-2026';
 
@@ -27,9 +27,6 @@ export default defineConfig({
   trailingSlash: SITE.trailingSlash ? 'always' : 'ignore',
   output: 'static',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap(),
     icon({
       include: {
@@ -65,6 +62,7 @@ export default defineConfig({
     domains: ['cdn.sanity.io'],
   },
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './src'),
